30ff248594665b70420b0cb9a6a2bcc0365677ca
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-16 17:18:39 -0700

    Fin Seccion 10

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,31 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    if(!valido) { return client.disconnect();}
+
+    usuarioConectado(uid);
+
+    /// Ingresar al usuario a un canal
+    //// canal general
+    client.join(uid);
+
+    // escuchar del cliente mensaje personal
+
+    client.on('mensaje-personal', async (payload) => {
+        await grabarMensaje(payload);
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
 
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
